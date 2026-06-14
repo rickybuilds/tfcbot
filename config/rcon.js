d2d854f6fd6269353eda7adbf1ef804f892b2e51
Ricky <84817582+venny2021@users.noreply.github.com>
2026-06-14 03:43:30 -0400

    added west to server votes

--- config/rcon.js
+++ config/rcon.js
@@ -52,6 +52,23 @@ if (process.env.TFC_RCON_CENTRAL_HOST) {
     logDir: process.env.HL_REMOTE_LOG_DIR_CENTRAL2 || process.env.HL_REMOTE_LOG_DIR,
     hltvDir: process.env.HL_REMOTE_HLTV_DIR_CENTRAL2 || process.env.HL_REMOTE_HLTV_DIR,
   };
+if (process.env.TFC_RCON_WEST_HOST) {
+  servers.west = {
+    name: "TFC West Server",
+    host: process.env.TFC_RCON_WEST_HOST,
+    port: parseInt(process.env.TFC_RCON_WEST_PORT || "27015", 10),
+    password: process.env.TFC_RCON_WEST_PASS,
+    url: "tinyurl.com/nonamewest",
+    ssh: {
+      host: process.env.HL_SSH_HOST_WEST || process.env.HL_SSH_HOST,
+      port: parseInt(process.env.HL_SSH_PORT || "22", 10),
+      user: process.env.HL_SSH_USER,
+      pass: process.env.HL_SSH_PASS,
+    },
+    logDir: process.env.HL_REMOTE_LOG_DIR_WEST || process.env.HL_REMOTE_LOG_DIR,
+    hltvDir: process.env.HL_REMOTE_HLTV_DIR_WEST || process.env.HL_REMOTE_HLTV_DIR,
+  };
+}
 }
 }
 module.exports = servers;
