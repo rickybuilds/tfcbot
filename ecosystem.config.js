996840621a5ddeddabd8980adff3e6c89c868f0f
venny2021 <84817582+venny2021@users.noreply.github.com>
2026-07-12 20:49:09 -0400

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
     cwd: "/root/tfcbot",
     env: {
       NODE_ENV: "production",
-      ONEVONE_ENABLED: "0",
+      ONEVONE_ENABLED: "1",
       ONEVONE_DRY_RUN: "1",
       ONEVONE_SERVER_SETUP_ENABLED: "0"
     }
